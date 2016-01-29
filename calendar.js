75db2439f752f9e55b90edba11fc115d36144ead
jason123 <jponce.sp@mysummitps.org>
2016-01-29 22:41:10 +0000

    update

---
+++ calendar.js
@@ -0,0 +1,122 @@
+var christmasEve = document.getElementById("christmasEve")
+var Info1 = document.getElementById("Info1")
+
+var christmasDay = document.getElementById("christmasDay")
+var Info1 = document.getElementById("Info2")
+
+var newyearsEve = document.getElementById("newyearsEve")
+var Info1 = document.getElementById("Info3")
+
+var thanksgivingDay = document.getElementById("thanksgivingDay")
+var Info2 = document.getElementById("Info4")
+
+var veteransDay = document.getElementById("veteransDay")
+var Info2 = document.getElementById("Info5")
+
+var electionDay = document.getElementById("electionDay")
+var Info2 = document.getElementById("Info6")
+
+var halloweenDay = document.getElementById("halloweenDay")
+var Info3 = document.getElementById("Info7")
+
+var colombusDay = document.getElementById("colombusDay")
+var Info3 = document.getElementById("Info8")
+
+var laborDay = document.getElementById("laborDay")
+var Info4 = document.getElementById("Info9")
+
+var independenceDay = document.getElementById("independenceDay")
+var Info5 = document.getElementById("Info10")
+
+var fathersDay = document.getElementById("fathersDay")
+var Info6 = document.getElementById("Info11")
+
+var memorialDay = document.getElementById("memorialDay")
+var Info7 = document.getElementById("Info12")
+
+var mothersDay = document.getElementById("mothersDay")
+var Info7 = document.getElementById("Info13")
+
+var easterDay = document.getElementById("easterDay")
+var Info8 = document.getElementById("Info14")
+
+var presidentsDay = document.getElementById("presidentsDay")
+var Info9 = document.getElementById("Info15")
+
+var valentinesDay = document.getElementById("valentinesDay")
+var Info9 = document.getElementById("Info16")
+
+var newyearDay = document.getElementById("newyearDay")
+var Info10 = document.getElementById("Info17")
+
+
+
+
+
+
+
+christmasEve.addEventListener("click", function(){
+    Info1.innerHTML = "Christmas Eve refers to the evening or entire day preceding Christmas Day, a widely celebrated festival commemorating the birth of Jesus of Nazareth."
+    })
+christmasDay.addEventListener("click", function(){
+    Info1.innerHTML = "Christmas or Christmas Day is an annual festival commemorating the birth of Jesus Christ, observed most commonly on December 25 as a religious and cultural celebration among billions of people around the world."
+})
+newyearsEve.addEventListener("click", function(){
+    Info1.innerHTML = "In the Gregorian calendar, New Year's Eve, the last day of the year, is on December 31."
+})
+
+thanksgivingDay.addEventListener("click", function(){
+    Info2.innerHTML = "It was originally celebrated as a day of giving thanks for the blessing of the harvest and of the preceding year."
+})
+
+veteransDay.addEventListener("click", function(){
+    Info2.innerHTML = "Veterans Day is an official United States public holiday, observed annually on November 11, that honors military veterans, that is, persons who served in the United States Armed Forces."
+})
+
+electionDay.addEventListener("click", function() {
+    Info2.innerHTML = "Election day refers to the day when general elections are held. In many countries, general elections are always held on a Sunday, to enable as many voters as possible to participate"
+})
+
+halloweenDay.addEventListener("click", function() {
+    Info3.innerHTML = "It begins observing Halloween, the time in the liturgical year dedicated to remembering the dead, including saints, martyrs, and all the faithful departed."
+})
+
+colombusDay.addEventListener("click", function() {
+    Info3.innerHTML = "Columbus Day is a national holiday in many countries in the Americas and elsewhere which officially celebrates the anniversary of Christopher Columbus' arrival and Discovery of America"
+})
+
+laborDay.addEventListener("click", function() {
+    Info4.innerHTML = "Labor Day in the United States is a public holiday celebrated on the first Monday in September."
+})
+
+independenceDay.addEventListener("click", function() {
+    Info5.innerHTML = ""
+})
+
+fathersDay.addEventListener("click", function() {
+    Info6.innerHTML = "Father's Day is a celebration honoring fathers and celebrating fatherhood, paternal bonds, and the influence of fathers in society."
+})
+
+memorialDay.addEventListener("click", function() {
+    Info7.innerHTML = "Memorial Day is a federal holiday in the United States for remembering the people who died while serving in the country's armed forces."
+})
+
+mothersDay.addEventListener("clcik", function() {
+    Info7.innerHTML = "Mother's Day is a modern celebration honoring one's mother, as well as motherhood, maternal bonds, and the influence of mothers in society."
+})
+
+easterDay.addEventListener("click", function() {
+    Info8.innerHTML = "Easter, also called Pasch or Resurrection Sunday, is a festival and holiday celebrating the resurrection of Jesus Christ from the dead"
+})
+
+presidentsDay.addEventListener("clcik", function() {
+    Info9.innerHTML = "Washington's Birthday is a United States federal holiday celebrated on the third Monday of February in honor of George Washington, the first President of the United States."
+})
+
+valentinesDay.addEventListener("click", function() {
+    Info9.innerHTML = "Valentine's Day, also known as Saint Valentine's Day or the Feast of Saint Valentine, is a celebration observed on February 14 each year."
+})
+
+newyearDay.addEventListener("click", function() {
+    Info10.innerHTML = "In the Gregorian calendar, New Year's Eve, the last day of the year, is on December 31."
+})
